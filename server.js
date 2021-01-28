c101de661d0e5d7ea3f9b501a2f668483f431d8c
Abhay-singh1 <54500897+Abhay-singh1@users.noreply.github.com>
2021-01-28 22:47:39 +0530

    Create server.js
    This is my main server file that i have added into my repository

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const connectDb = require('C:\\Users\\abhay\\FIRSTAPP\\config\\db.js');
+const os = require('os');
+const fs = require('fs');
+const app = express();
+connectDb();
+
+app.use(express.json({extended: false}));
+app.get('/',(req,res) => {
+    res.send('API RUNNING')
+});
+
+app.use('/api/users', require('./routes/api/users'));
+console.log('n error!')
+
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+const PORT=  process.env.PORT || 3300;
+
+app.listen(PORT, () => console.log(`Server started on port :${PORT}`));
+
+
